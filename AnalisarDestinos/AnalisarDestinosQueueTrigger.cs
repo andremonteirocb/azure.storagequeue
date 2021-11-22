115519fde93b51a6d7dacedbbc2bf5631b243fa7
Andre Luis Soares Alves Monteiro <andre.monteiro@phidelis.com.br>
2021-11-22 16:01:03 -0300

    ...

--- AnalisarDestinos/AnalisarDestinosQueueTrigger.cs
+++ AnalisarDestinos/AnalisarDestinosQueueTrigger.cs
@@ -16,7 +16,7 @@ namespace Fundamentos.Azure.StorageQueue.AnalisarDestinos
             var logger = context.GetLogger("AnalisarDestinosQueueTrigger");
             logger.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
 
-            var connectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+            var connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
 
             var queueName = "notificaremails"; 
             var queue = new QueueClient(connectionString, queueName, new QueueClientOptions { MessageEncoding = QueueMessageEncoding.Base64 });
